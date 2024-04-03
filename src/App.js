b698beecd2ab161f66a25e7e11433b74b7d96f76
pallavigowda2001 <pallavigowda78197@gmail.com>
2024-04-03 13:59:31 +0530

    redux-crud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import Menu from './Component/Menu'
+import Home from './Pages/Home'
+import Update from './Pages/Update'
+import Pnf from './Pages/Pnf'
+import Create from './Pages/Create'
 
-function App() {
+
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <BrowserRouter>
+      <Menu/>
+          <Routes>
+            <Route path={`/`} element ={<Home/>}/>
+            <Route path={`/create`} element ={<Create/>}/>
+
+
+            <Route path={`/edit`} element ={<Update/>}/>
+            <Route path={`/*`} element ={<Pnf/>}/>
+
+
+           </Routes>
+   </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
